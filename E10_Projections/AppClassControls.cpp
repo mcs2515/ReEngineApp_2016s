db16ee34bd621156b0c77e1c41410f2dd0c6fddb
mcs2515 <mcs2515@rit.edu>
2017-03-23 00:28:31 -0400

    adding E10 Projections

--- E10_Projections/AppClassControls.cpp
+++ E10_Projections/AppClassControls.cpp
@@ -20,28 +20,42 @@ void AppClass::ProcessKeyboard(void)
 	//F1 Controllers
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::F1))
 	{
+		//fieled of view, aspect ratio, near clipping plane (length of it), far clipping plane (length of it)
 		m_m4Projection = glm::perspective(45.0f, 1080.0f / 768.0f, 0.01f, 1000.0f);
+		//camera position, looking at, camera orientation
 		m_m4View = glm::lookAt(glm::vec3(0.0f, 0.0f, 15.0f), glm::vec3(0.0f, 0.0f, 14.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	}
 
 	//F2 Controllers
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::F2))
 	{
+		m_m4Projection = glm::perspective(45.0f, 1080.0f / 768.0f, 0.01f, 1000.0f);
+		m_m4View = glm::lookAt(glm::vec3(15.0f, 0.0f, 0.0f), glm::vec3(14.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 	}
 
 	//F3 Controllers
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::F3))
 	{
+		//far clipping anywhere between 15-17 units bc the first object is at 14 units in Z
+		//the second object starts at around 18 units in the z
+		m_m4Projection = glm::perspective(45.0f, 1080.0f / 768.0f, 0.01f, 17.0f);
+		m_m4View = glm::lookAt(glm::vec3(0.0f, 0.0f, 15.0f), glm::vec3(0.0f, 0.0f, 14.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	}
 
 	//F4 Controllers
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::F4))
 	{
+		m_m4Projection = glm::perspective(45.0f, 1080.0f / 768.0f, 0.01f, 1000.0f);
+		m_m4View = glm::lookAt(glm::vec3(15.0f, 0.0f, 0.0f), glm::vec3(14.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	}
 
 	//F5 Controllers
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::F5))
 	{
+		//camera bounderies for orthographic
+		//left, right, bottom, top, near, far
+		m_m4Projection = glm::ortho(-12.0f, 12.0f, -25.0f, 25.0f, 0.1f, 1000.0f );
+		m_m4View = glm::lookAt(glm::vec3(15.0f, 0.0f, 0.0f), glm::vec3(14.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	}
 
 	//Exit the program
