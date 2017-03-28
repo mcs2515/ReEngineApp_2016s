e0c97ddf6fe6d2405a10dcc8629bdc284393708c
Megan Smith (RIT Student) <mcs2515@ad.rit.edu>
2017-03-28 11:17:27 -0400

    adding PracticeMidterm

---
+++ E11_MidtermPractice/AppClass.cpp
@@ -0,0 +1,64 @@
+#include "AppClass.h"
+void AppClass::Update(void)
+{
+#pragma region DOES NOT NEED CHANGES
+	//Update the system's time
+	m_pSystem->UpdateTime();
+
+	//Update the mesh manager's time without updating for collision detection
+	m_pMeshMngr->Update();
+#pragma endregion
+#pragma region FEEL FREE TO USE THIS CLOCK
+	//Calculate delta and total times
+	static double dTotalTime = 0.0; //Total time of the simulation
+	double dDeltaTime = m_pSystem->LapClock(); //time difference between function calls
+	dTotalTime += dDeltaTime; //Incrementing the time differences 
+#pragma endregion
+#pragma region YOUR CODE GOES HERE
+	m_m4Steve = glm::mat4(1.0f); // same as m_m4Steve = IDENTITY_M4; setting the identity to steve
+
+	float rPercentage = MapValue(static_cast<float>(dTotalTime), 0.0f, 5.0f, 0.0f, 360.0f); //map 0-5sec to a scale of 0-360 (1 full circle in 5 secs)
+
+    //rotate steve
+	m_m4Steve = glm::rotate(rPercentage, vector3(0.0f, 0.0f, 1.0f));
+
+	//switch on and off the reverse base on last value
+	if (dTotalTime > 5.0f) {
+		//reset time
+		dTotalTime = 0.0f;
+
+		if (!reverse) {
+			reverse = true;
+		}
+		else {
+			reverse = false;
+		}
+	}
+
+	float tPercentage = MapValue(static_cast<float>(dTotalTime), 0.0f, 5.0f, 0.0f, 1.0f); //map 0-5sec to a scale of 0-1 (5 units out/in in 5 secs)
+
+	//add the translation to the rotation
+	if (!reverse) {
+		m_m4Steve *= glm::translate(glm::lerp(vector3(0.0f, 0.0f, 0.0f), vector3(0.0f, 5.0f, 0.0f), tPercentage));
+	}
+	else {
+		m_m4Steve *= glm::translate(glm::lerp(vector3(0.0f, 5.0f, 0.0f), vector3(0.0f, 0.0f, 0.0f), tPercentage));
+	}
+#pragma endregion
+#pragma region DOES NOT NEED CHANGES
+	//Set the model matrix
+	m_pMeshMngr->SetModelMatrix(m_m4Steve, "Steve");
+	
+	//Adds all loaded instance to the render list
+	m_pMeshMngr->AddInstanceToRenderList("ALL");
+
+	//Indicate the FPS
+	int nFPS = m_pSystem->GetFPS();
+	//Print info on the screen
+	m_pMeshMngr->PrintLine("\n" + m_pSystem->GetAppName(), REYELLOW);
+	m_pMeshMngr->Print("Seconds:");
+	m_pMeshMngr->PrintLine(std::to_string(dTotalTime), RERED);
+	m_pMeshMngr->Print("FPS:");
+	m_pMeshMngr->Print(std::to_string(nFPS), RERED);
+#pragma endregion
+}
